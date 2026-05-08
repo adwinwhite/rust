1ef22653b0ea3147cb2ee70533e61006529855b2
Adwin White <adwinw01@gmail.com>
2026-05-08 11:03:52 +0800

    add a new folder for ambiguous alias normalization

--- compiler/rustc_next_trait_solver/src/normalize.rs
+++ compiler/rustc_next_trait_solver/src/normalize.rs
@@ -3,8 +3,8 @@ use rustc_type_ir::inherent::*;
 use rustc_type_ir::solve::{Goal, NoSolution};
 use rustc_type_ir::{
     self as ty, Binder, FallibleTypeFolder, InferConst, InferCtxtLike, InferTy, Interner,
-    TypeFoldable, TypeSuperFoldable, TypeSuperVisitable, TypeVisitable, TypeVisitableExt,
-    TypeVisitor, UniverseIndex,
+    TypeFlags, TypeFoldable, TypeSuperFoldable, TypeSuperVisitable, TypeVisitable,
+    TypeVisitableExt, TypeVisitor, UniverseIndex,
 };
 use tracing::instrument;
 
@@ -258,5 +258,104 @@ where
             .0
             .expect_const())
         }
+    }
+}
+
+// Only handle ambiguous alias in the outmost instantiated binder.
+pub struct BinderRenormalizer<'a, Infcx, I, F>
+where
+    Infcx: InferCtxtLike<Interner = I>,
+    I: Interner,
+{
+    infcx: &'a Infcx,
+    stalled_goals: Vec<Goal<I, I::Predicate>>,
+    normalize: F,
+}
+
+impl<'a, Infcx, I, F> BinderRenormalizer<'a, Infcx, I, F>
+where
+    Infcx: InferCtxtLike<Interner = I>,
+    I: Interner,
+    F: FnMut(I::Term) -> Result<(I::Term, Option<Goal<I, I::Predicate>>), NoSolution>,
+{
+    pub fn new(infcx: &'a Infcx, stalled_goals: Vec<Goal<I, I::Predicate>>, normalize: F) -> Self {
+        Self { infcx, stalled_goals, normalize }
+    }
+
+    pub fn stalled_goals(self) -> Vec<Goal<I, I::Predicate>> {
+        self.stalled_goals
+    }
+
+    fn normalize_alias_term(&mut self, alias_term: I::Term) -> Result<I::Term, NoSolution> {
+        let (normalized, ambig_goal) = (self.normalize)(alias_term)?;
+
+        self.stalled_goals.extend(ambig_goal);
+        Ok(normalized)
+    }
+}
+
+impl<'a, Infcx, I, F> FallibleTypeFolder<I> for BinderRenormalizer<'a, Infcx, I, F>
+where
+    Infcx: InferCtxtLike<Interner = I>,
+    I: Interner,
+    F: FnMut(I::Term) -> Result<(I::Term, Option<Goal<I, I::Predicate>>), NoSolution>,
+{
+    type Error = NoSolution;
+
+    fn cx(&self) -> I {
+        self.infcx.cx()
+    }
+
+    fn try_fold_binder<T: TypeFoldable<I>>(
+        &mut self,
+        t: Binder<I, T>,
+    ) -> Result<Binder<I, T>, Self::Error> {
+        // We don't look into nested binders since they're not instantiated.
+        // Thus we assume we won't meet escaping bound vars as well.
+        Ok(t)
+    }
+
+    #[instrument(level = "trace", skip(self), ret)]
+    fn try_fold_ty(&mut self, ty: I::Ty) -> Result<I::Ty, Self::Error> {
+        debug_assert!(!ty.has_escaping_bound_vars());
+        if !ty.has_type_flags(TypeFlags::HAS_AMBIGUOUS_ALIAS) {
+            return Ok(ty);
+        }
+
+        // With eager normalization, we should normalize the args of alias before
+        // normalizing the alias itself.
+        let ty = ty.try_super_fold_with(self)?;
+        let ty::Alias(ty::AliasTy { kind: ty::AliasTyKind::Ambiguous { def_id }, args, .. }) =
+            ty.kind()
+        else {
+            return Ok(ty);
+        };
+
+        let original_alias = I::Ty::new_alias(
+            self.cx(),
+            ty::AliasTy::new_from_args(
+                self.cx(),
+                ty::AliasTyKind::new_from_def_id(self.cx(), def_id),
+                args,
+            ),
+        );
+        Ok(ensure_sufficient_stack(|| self.normalize_alias_term(original_alias.into()))?
+            .expect_ty())
+    }
+
+    #[instrument(level = "trace", skip(self), ret)]
+    fn try_fold_const(&mut self, ct: I::Const) -> Result<I::Const, Self::Error> {
+        debug_assert!(!ct.has_escaping_bound_vars());
+        if !ct.has_type_flags(TypeFlags::HAS_AMBIGUOUS_ALIAS) {
+            return Ok(ct);
+        }
+
+        // With eager normalization, we should normalize the args of alias before
+        // normalizing the alias itself.
+        let ct = ct.try_super_fold_with(self)?;
+        let ty::ConstKind::Unevaluated(..) = ct.kind() else { return Ok(ct) };
+        // FIXME: figure out how to handle const that contains ambiguous alias. How can it do so?
+        // Can constant have args or lifetime? that's kinda ridiculus.
+        Ok(ct)
     }
 }
